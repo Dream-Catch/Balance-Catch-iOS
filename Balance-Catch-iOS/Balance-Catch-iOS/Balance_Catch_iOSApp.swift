2249fce04966e79e1a0f217896523d0d58a68a88
Minny27 <dltmdals0608@naver.com>
2023-03-14 09:56:18 +0900

    Add: 프로젝트 파일 생성 - .gitignore 파일 생성

---
+++ Balance-Catch-iOS/Balance-Catch-iOS/Balance_Catch_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Balance_Catch_iOSApp.swift
+//  Balance-Catch-iOS
+//
+//  Created by SeungMin on 2023/03/14.
+//
+
+import SwiftUI
+
+@main
+struct Balance_Catch_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Balance-Catch-iOS/Balance-Catch-iOSUITests/Balance_Catch_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Balance_Catch_iOSUITestsLaunchTests.swift
+//  Balance-Catch-iOSUITests
+//
+//  Created by SeungMin on 2023/03/14.
+//
+
+import XCTest
+
+class Balance_Catch_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
