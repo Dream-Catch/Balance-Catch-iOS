c790f9c8a330e0c2d429610525a76d0d37a8be38
Minny27 <dltmdals0608@naver.com>
2023-04-11 20:12:51 +0900

    [#23]Feat: 랜덤 선택 버튼 클릭 시, 한 바퀴 돌고 선택된 질문으로 scroll되도록 구현 - SelectQuestionView   - 강제 언래핑 없앰   - reset 버튼 클릭 이벤트 처리 함수화   - reset 버튼 클릭 지속시간 3초로 늘림   - 필요없는 개행 삭제 - QuestionPickerView   - randomPickAnimation 프로토콜 구현 -> 사용할 함수 직관적으로 알 수 있음   - 처음 -> 끝 -> 선택된 질문 순으로 순차적으로 스크롤되도록 시차를 설정   - 각 작업이 순차적으로 진행되도록 async await사용 -> 버벅이는 것을 없애기 위함

--- Balance-Catch-iOS/Balance-Catch-iOS/Custom/WheelStylePicker/QuestionPickerView.swift
+++ Balance-Catch-iOS/Balance-Catch-iOS/Custom/WheelStylePicker/QuestionPickerView.swift
@@ -7,8 +7,17 @@
 
 import SwiftUI
 
+protocol randomPickAnimation {
+    func scrollToEndIndex() async
+    func scrollToZeroIndex() async
+    func scroll(to index: Int) async
+}
+
 struct QuestionPickerView: View {
     let questions: [Question]
+    let isRandomPick: Bool
+    let serialQueue = DispatchQueue(label: "serialQueue")
+    
     @Binding var selectedIndex: Int
     
     var body: some View {
@@ -25,17 +34,50 @@ struct QuestionPickerView: View {
         }
         .onAppear() {
             let endIndex = selectedIndex
-            selectedIndex = 0
-            for index in 0...endIndex {
-                DispatchQueue.main.asyncAfter(deadline: .now() + Double(index) / 20) {
-                    withAnimation {
-                        selectedIndex = index
-                    }
+            
+            if isRandomPick {
+                Task {
+                    await scrollToEndIndex()
+                    await scrollToZeroIndex()
+                    await scroll(to: endIndex)
                 }
             }
         }
         .pickerStyle(WheelPickerStyle())
         .frame(height: 200)
         .padding([.leading, .trailing], 20)
+    }
+}
+
+extension QuestionPickerView: randomPickAnimation {
+    
+    func scrollToEndIndex() async {
+        selectedIndex = 0
+        for index in 0..<questions.count {
+            serialQueue.asyncAfter(deadline: .now() + Double(index) / 25) {
+                withAnimation {
+                    selectedIndex = index
+                }
+            }
+        }
+    }
+    
+    func scrollToZeroIndex() async {
+        serialQueue.asyncAfter(deadline: .now() + Double(questions.count) / 25 + 0.3) {
+            withAnimation {
+                selectedIndex = 0
+            }
+        }
+    }
+    
+    func scroll(to index: Int) async {
+        let totalEndTime: Double = Double(questions.count) / 25 + 0.6
+        for i in 0...index {
+            serialQueue.asyncAfter(deadline: .now() + totalEndTime + Double(i) / 25) {
+                withAnimation {
+                    selectedIndex = i
+                }
+            }
+        }
     }
 }
