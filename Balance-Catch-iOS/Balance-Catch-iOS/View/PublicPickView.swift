f91e5ef70f9213f7f84c792d8d7bc058b44242d5
eundeang <2021112027@dgu.ac.kr>
2023-04-14 12:12:13 +0900

    막대 변하지 않는 오류 수정

--- Balance-Catch-iOS/Balance-Catch-iOS/View/PublicPickView.swift
+++ Balance-Catch-iOS/Balance-Catch-iOS/View/PublicPickView.swift
@@ -13,13 +13,13 @@ struct PublicPickView: View {
     @State private var firstIncreAmount : Double = 0.0
     @State private var secondIncreAmount : Double = 0.0
     
-    @State var firstQuestion = (question: "잠수이별", amount: 60.0)
-    @State var secondQuestion = (question: "환승이별", amount: 40.0)
+    @State var firstQuestion = (question: "잠수이별", amount: 10.0)
+    @State var secondQuestion = (question: "환승이별", amount: 60.0)
     
     
   
     
-//    func whatIsPick(firstPer:Int,secondPer:Int )->Double
+//    func whatIsWinner(firstPer:Int ,secondPer:Int )->(
 //    {
 //        if firstAmount>secondAmount {
 //            return firstAmount
@@ -28,6 +28,7 @@ struct PublicPickView: View {
 //            return secondAmount
 //        }
 //    }
+    
     
     let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
     
@@ -68,7 +69,7 @@ struct PublicPickView: View {
                 .shadow(color: .black.opacity(0.25), radius: 20, x: 0, y: 100)
             )
             .onReceive(timer) { _ in
-                if firstIncreAmount < firstQuestion.amount {
+                if firstIncreAmount < (firstQuestion.amount > secondQuestion.amount ? firstQuestion.amount : secondQuestion.amount) {
                     firstIncreAmount += 8
                 }
                 if animationAmount < 1.2 {
@@ -117,7 +118,7 @@ struct PublicPickView: View {
             .scaleEffect(1)
             .animation(.easeIn(duration: 1).delay(1), value: animationAmount)
             .onReceive(timer) { _ in
-                if secondIncreAmount < secondQuestion.amount {
+                if secondIncreAmount < (firstQuestion.amount > secondQuestion.amount ? secondQuestion.amount : firstQuestion.amount) {
                     secondIncreAmount += 8
                 }
             }
