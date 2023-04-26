e23f56e906daaf4ca9ccf4dbba134ffbcc69f5e6
Minny27 <dltmdals0608@naver.com>
2023-04-26 18:51:56 +0900

    [#31]Bug: 질문 랜덤 배정 버튼 클릭 후 바로 next를 눌렀을 때 텍스트가 랜덤으로 바뀌는 현상 수정 - 재시도 버튼 클릭 시, Next버튼 3초 비활성화 - 질문 선택 화면에 들어왔을 때, 재시도 버튼을 바로 누를 수 없도록 처음에도 3초 비활성화 - 필요없는 개행 삭제 - 필요없는 id설정 삭제 - 삼항 연산자를 if, else문으로 변경 -> 가독성을 위함

--- Balance-Catch-iOS/Balance-Catch-iOS/Custom/WheelStylePicker/QuestionPickerView.swift
+++ Balance-Catch-iOS/Balance-Catch-iOS/Custom/WheelStylePicker/QuestionPickerView.swift
@@ -29,7 +29,6 @@ struct QuestionPickerView: View {
                             selectedIndex = index
                         }
                     }
-                    .id(questions[index])
             }
         }
         .onAppear() {
